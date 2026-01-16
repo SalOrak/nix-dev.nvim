be86b2109d289ef8c08f7fca55bc7db07ee2c852
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-01-16 17:53:09 +0100

    :)

---
+++ README.md
@@ -0,0 +1,8 @@
+# nix-dev.nvim
+
+Nix Develop for Neovim. Set all the environments appropiately with one command.
+
+
+
+### Acknowledgements
+[Nix-develop.nvim](https://github.com/figsoda/nix-develop.nvim) highly inspired by the work of [figsoda](https://github.com/figsoda).
---
+++ lua/nix-dev/init.lua
@@ -0,0 +1,118 @@
+local M = {
+	command = "nix print-dev-env --extra-experimental-features 'nix-command flake' --json"
+}
+
+M.ignored_variables = {
+  BASHOPTS,
+  HOME,
+  NIX_BUILD_TOP,
+  NIX_ENFORCE_PURITY,
+  NIX_LOG_FD,
+  NIX_REMOTE,
+  PPID,
+  SHELL,
+  SHELLOPTS,
+  SSL_CERT_FILE,
+  TEMP,
+  TEMPDIR,
+  TERM,
+  TMP,
+  TMPDIR,
+  TZ,
+  UID,
+}
+
+M.PATH_VARS = {
+	PATH = ":",
+	XDG_DATA_DIRS = ":",
+}
+
+
+M.EVENTS = {
+	POST = "NixDevPost",
+}
+
+--- Fires an autocommand related to NixDev.
+---@param pattern M.events Patter to execute
+---@param data table Data to pass to the command
+local exec_autocmd = function(pattern, data)
+	vim.api.nvim_exec_autocmds("User", {
+		pattern = pattern,
+		data = data
+	})
+end
+
+M.nix_develop = function()
+	local opts = {output = "", stdout = vim.uv.new_pipe()}
+
+	local cwd = vim.uv.cwd()
+	vim.uv.chdir("/home/hector/personal/acidburn/")
+	local cmd = vim.split(M.command, " ")
+
+	vim.system(cmd, {text =true}, function(obj)
+		if obj.code ~= 0 then
+			vim.api.nvim_notify(string.format("[ERROR] Failed to execute with code %d", obj.code), vim.log.levels.ERROR)
+			return
+		end
+		local stdout = obj.stdout
+		local ok, data= pcall(vim.json.decode, stdout)
+
+		if not ok then
+			print("Error, not ok")
+			exec_autocmd(M.EVENTS.POST,{
+				path = vim.uv.cwd(),
+				errmsg = "[ERROR] Could not decode output from command"
+				error = true,
+			})
+			return
+		end
+
+		local vars  = data["variables"]
+
+		if not vars then
+			exec_autocmd(M.EVENTS.POST,{
+				path = vim.uv.cwd(),
+				errmsg = "[ERROR] No 'variables' found"
+				error = true,
+			})
+			return
+		end
+
+		for envName, data in pairs(vars) do
+			local should_ignore = vim.tbl_contains(M.ignored_variables, envName)
+			should_ignore = should_ignore or data.type ~= "exported"
+
+			if should_ignore then return end
+
+			local sep = M.PATH_VARS[envName]
+			
+			-- Check if the env variable is a PATH type
+			if sep then
+				local path = vim.uv.os_getenv(envName)
+				if path then
+					vim.uv.os_setenv(envName, data.value .. sep .. path)
+				end
+			else
+				vim.uv.os_setenv(envName, data.value)
+			end
+		end
+	end)
+			exec_autocmd(M.EVENTS.POST,{
+				path = vim.uv.cwd(),
+				msg = "[INFO] Succesfully activated environment"
+			})
+
+	vim.uv.chdir(cwd)
+end
+
+
+
+M.setup = function(opts)
+	M.ignored_variables = vim.tbl_deep_extend('force', M.ignored_variables, opts.ignored_variables)
+	M.PATH_VARS = vim.tbl_deep_extend('force', M.PATH_VARS, opts.path_vars)
+	M.command = opts.command or M.command
+
+end
+
+
+return M
